e3bd5d9c11fa0cff2257ee389fa3670869362a13
Ayelen-Ceresola <ayeceresola@hotmail.com>
2023-04-28 09:32:53 -0300

    Firts commit

---
+++ ejercicio.js
@@ -0,0 +1,16 @@
+let miNombre = "Ayelen"
+let miApellido = "Ceresola"
+let miEdad = 37
+ let miMascota = "Bronco"
+ let edadDeMascota = "10"
+
+console.log (miNombre)
+
+const nombreCompleto = ` ${miNombre} ${miApellido}`
+console.log (nombreCompleto)
+
+const textoPresentacion = ` ${miNombre} ${miApellido} ${miEdad} ${miMascota} ${edadDeMascota}`
+console.log (textoPresentacion)
+
+
+
